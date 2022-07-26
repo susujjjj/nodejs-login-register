d96fd5d64751ae8299e26e657226d03e6a86788d
sbj2 <sbj@ryencatchers.com>
2022-07-26 17:02:08 +0900

    write HTML using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 'use strict'
 
-console.log('hello')
-console.log('bye')
+const id = document.querySelector('#id'),
+  password = document.querySelector('#password'),
+  loginBtn = document.querySelector('button')
+
+loginBtn.addEventListener('click', login) //이 두번째 파라미터로 넘어오는것은 함수
+
+function login() {
+  const req = {
+    id: id.value,
+    password: password.value,
+  }
+  console.log('bye')
+}
